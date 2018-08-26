0cfd2c038f0a4aecac3e8a7b21a4320bfb260718
Anthony Gonzalez <agonzale34@gmail.com>
2018-08-26 13:34:26 -0500

    first commit

---
+++ src/test/mines/models/Square.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test.mines.models;
+
+/**
+ *
+ * @author Anthony Gonzalez
+ */
+public class Square {
+    
+    boolean covered;
+    
+    boolean hasMinesAround;
+    
+    boolean hasFlag;
+    
+    boolean hasMine;
+    
+    int nMinesAround;
+    
+}
